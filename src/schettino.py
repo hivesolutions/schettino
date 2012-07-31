11bcf1293102b3b8da3eb6ec69a9b84bcfdebd47
João Magalhães <joamag@hive.pt>
2012-07-31 07:07:06 +0100

    better partial solution

--- src/schettino.py
+++ src/schettino.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-PERSONS = ["Ana", "Sofia", "Alexandra", "Rabeta"]
+PERSONS = ["Ana", "Tobias", "Matias", "Rabeton"]
 """ The persons to be allocated to the tasks
 this are the names for the domain ranges """
 
@@ -57,9 +57,21 @@ BITMAP = (
 """ The base bitmap that controls the scheduling
 of the task in a per time basis """
 
-N_DAYS = 7
 
-N_HOURS = 12
+#BITMAP = (
+#    (1, 0),
+#    (1, 1),
+#    (0, 0)
+#)
+
+
+N_DAYS = len(BITMAP)
+""" The number of days in the problem, this value
+is previously calculated for performance reasons """
+
+N_HOURS = len(BITMAP[0])
+""" The number of hours in the problem, this value
+is previously calculated for performance reasons """
 
 def rule_1(solution):
     """
@@ -118,7 +130,7 @@ def rules(solution):
 
     return True
 
-def solve(solution, value = None, i = 0, j = 0):
+def solve(solution, value = None, i = 0, j = 0, all = False, callback = None):
     # tenho de alocar para x dias do mes
     # vou primeiro alocar para uma semana
 
@@ -131,29 +143,34 @@ def solve(solution, value = None, i = 0, j = 0):
     # varias pessoas (unidade de alocacao) ao trabalho
 
     if not value == None:
-        if j == N_HOURS: i += 1; j = 0
-        if i == N_DAYS: return True
-
-        if BITMAP[i][j] == 0:
-            solution[i][j] = -1
-        else:
-            solution[i][j] = value
-            result = rules(solution)
-            if not result: return False
+        solution[i][j] = value
+        result = rules(solution)
+        if not result: return False
 
         j += 1
 
-    for index in range(PERSONS_COUNT):
-        result = solve(solution, index, i, j)
-        if result: return result
+        if j == N_HOURS: i += 1; j = 0
+        if i == N_DAYS: callback and callback(solution); return True
+
+    if BITMAP[i][j] == 0:
+        result = solve(solution, -1, i, j, all, callback)
+        if all: pass
+        elif result: return result
+    else:
+        for index in range(PERSONS_COUNT):
+            result = solve(solution, index, i, j, all, callback)
+            if all: continue
+            if not result: continue
+            return result
 
     return False
 
+def tobias(solution):
+    print_solution(solution)
+
 def run():
     solution = [[-1 for _value in xrange(N_HOURS)] for _value in xrange(N_DAYS)]
-    solved = solve(solution)
-    print solved
-    print_solution(solution)
+    solve(solution, all = True, callback = tobias)
 
 def print_solution(solution):
 
